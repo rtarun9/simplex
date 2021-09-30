e68fe02861225252bdf22f6c2810fd1700cfb995
Tarun Ramaswamy <TarunRamaswamy@outlook.com>
2021-09-30 20:13:46 +0530

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simplex
-Simple software - ray tracer make with no third party Api's.
+Simple software - ray tracer make with no third party Api's. Based on Peter Shirley's Ray tracing in a weekend book.
+
+# building
+Use the build.bat file in the bin directory to build + run the executable. The output PPM file will also be in the directory.
---
+++ bin/CMakeFiles/simplex.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplex.dir/src/main.cpp.obj"
+  "CMakeFiles/simplex.dir/src/main.cpp.obj.d"
+  "libsimplex.dll.a"
+  "simplex.exe"
+  "simplex.exe.manifest"
+  "simplex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simplex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
